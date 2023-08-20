51e6419516e46fd6ba5c0493a9b2892631554045
cafe small house <ybaram40@gmail.com>
2023-08-20 20:12:24 +0900

    react-pdf 사용

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,17 @@
+import { Document, Page, pdfjs } from 'react-pdf';
+
 const App = () => {
+  pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.js', import.meta.url).toString();
   return (
-    <>
-      <div></div>
-    </>
+    <Document
+      file={'http://localhost:5173/혼인관계증명서.pdf'}
+      onLoadSuccess={({ numPages }) => {
+        console.log('Document loaded with ' + numPages + ' pages');
+      }}
+      options={{ workerSrc: '/pdf.worker.js' } as any}
+    >
+      <Page pageNumber={1} />
+    </Document>
   );
 };
 
